de52a6166c70cb93b741af44e9c1d191a50b9658
yunu.kim <kl1467@gmail.com>
2018-02-02 15:32:03 +0900

    default function & test module

--- ccorgi/Assets/Corgi.cs
+++ ccorgi/Assets/Corgi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Corgi : MonoBehaviour
@@ -28,15 +29,17 @@ public class Corgi : MonoBehaviour
     }
 
     public delegate void ResolveAction(Texture2D texture);
-    public delegate void RejectAction(string errorMessage);
+    public delegate void FallbackAction(string url, int version);
     public delegate void CorgiAction(string url, int version, ResolveAction resolve);
 
     public int preload_capacity = 10;
     public int memory_capacity = 50;
     public int disk_capacity = 500;
+    public FallbackAction fallback_delegate;
 
-    Dictionary<string, CorgiChunk> memoryCache;
-    Dictionary<string, CorgiChunk> diskCache;
+    const string DISK_CACHE_PATH = "diskcache.json";
+    Dictionary<string, Texture2D> memoryCache = new Dictionary<string, Texture2D>();
+    Dictionary<string, string> diskCache = new Dictionary<string, string>();
 
     public enum CorgiLayer
     {
@@ -46,14 +49,60 @@ public class Corgi : MonoBehaviour
         WEB,
     }
 
-    public static void Fetch(string url, int version, ResolveAction resolve, RejectAction reject)
+    void Awake()
+    {
+        if(_instance == null)
+            _instance = this;
+
+        var path = Path.Combine(Application.persistentDataPath, DISK_CACHE_PATH);
+        var content = File.ReadAllText(path);
+        if(string.IsNullOrEmpty(content))
+            diskCache = JsonUtility.FromJson<Dictionary<string, string>>(content);
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (!pauseStatus)
+            return;
+
+        var path = Path.Combine(Application.persistentDataPath, DISK_CACHE_PATH);
+        string content = JsonUtility.ToJson(diskCache);
+        File.WriteAllText(path, content);
+    }
+
+    void OnDestroy()
+    {
+        var path = Path.Combine(Application.persistentDataPath, DISK_CACHE_PATH);
+        string content = JsonUtility.ToJson(diskCache);
+        File.WriteAllText(path, content);
+    }
+
+    public static void Fetch(string url, int version, ResolveAction resolve, FallbackAction reject)
     {
         instance._Fetch(url, version, resolve, reject);
     }
 
-    public void _Fetch(string url, int version, ResolveAction resolve, RejectAction reject)
+    public void _Fetch(string url, int version, ResolveAction resolve, FallbackAction reject)
     {
-
+        Texture2D tex;
+        string path;
+        if (memoryCache.TryGetValue(url, out tex))
+        {
+            Debug.Log("Memory hit");
+            resolve(tex);
+        }
+        else if (diskCache.TryGetValue(url, out path))
+        {
+            StartCoroutine(DownloadLocal(url, version, path, resolve, reject));
+        }
+        else
+        {
+            StartCoroutine(DownloadURL(url, version, (bytes, texture) =>
+            {
+                resolve(texture);
+                SaveFile(bytes, url);
+            }, reject));
+        }
     }
 
     public static void AddCacheLayer(int priority, CorgiAction action)
@@ -84,5 +133,65 @@ public class Corgi : MonoBehaviour
     public void _Memorize()
     {
 
+    }
+
+    void SaveFile(byte[] bytes, string url)
+    {
+        var path = Path.Combine(Application.temporaryCachePath, Path.GetRandomFileName());
+        diskCache[url] = path;
+        var t = new System.Threading.Thread(() =>
+        {
+            File.WriteAllBytes(path, bytes);
+        });
+        t.Start();
+    }
+
+    IEnumerator DownloadURL(string url, int version, Action<byte[], Texture2D> callback, FallbackAction reject)
+    {
+        var www = new WWW(url);
+        yield return www;
+        if(!string.IsNullOrEmpty(www.error))
+        {
+            reject(url, version);
+            fallback_delegate(url, version);
+        }
+        else
+        {
+            Debug.Log("Web hit");
+            Texture2D tex;
+            if (!memoryCache.TryGetValue(url, out tex))
+            {
+                tex = new Texture2D(0, 0);
+                memoryCache.Add(url, tex);
+            }
+            www.LoadImageIntoTexture(tex);
+            callback(www.bytes, tex);
+        }
+    }
+
+    IEnumerator DownloadLocal(string url, int version, string path, ResolveAction resolve, FallbackAction reject)
+    {
+        var www = new WWW("file:///" + path);
+        yield return www;
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            yield return DownloadURL(url, version, (bytes, texture) =>
+            {
+                resolve(texture);
+                SaveFile(bytes, url);
+            }, reject);
+        }
+        else
+        {
+            Debug.Log("Disk hit");
+            Texture2D tex;
+            if (!memoryCache.TryGetValue(url, out tex))
+            {
+                tex = new Texture2D(0, 0);
+                memoryCache.Add(url, tex);
+            }
+            www.LoadImageIntoTexture(tex);
+            resolve(tex);
+        }
     }
 }
---
+++ ccorgi/Assets/TestModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestModule : MonoBehaviour
+{
+    public List<string> urls;
+    public RawImage targetImg;
+    private int index = -1;
+
+    private DateTime startTime;
+
+	void Start ()
+    {
+        ChangeImageIndex(0);
+    }
+
+    public void NextUrl()
+    {
+        ChangeImageIndex((index + 1) % urls.Count);
+    }
+
+    void ChangeImageIndex(int _index)
+    {
+        if (index == _index)
+            return;
+
+        index = _index;
+        startTime = DateTime.Now;
+        Corgi.Fetch(urls[index], 0, OnFetchDone, OnFetchFailed);
+    }
+
+
+    void OnFetchDone(Texture2D tex)
+    {
+        Debug.Log("Timespan : " + (DateTime.Now - startTime).TotalMilliseconds + " ms");
+        targetImg.texture = tex;
+    }
+
+    void OnFetchFailed(string url, int version)
+    {
+        Debug.Log("Fetch Faield " + url + " v" + version);
+    }
+}
